9bbe2e2c7f5c72632f7f3e0d4af57940b6e0b487
akosijovi <jftorrevillas@gmail.com>
2018-10-15 21:23:28 +0800

    Stores result when button is clicked

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -13,6 +13,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 15" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 10" clicked={this.props.onSubtractCounter}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -20,7 +27,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.result
     };
 };
 
@@ -30,6 +38,8 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
         onAddCounter: () => dispatch({type: 'ADD', val: 15}),
         onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 10}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT'}),
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,6 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    result: []
 }
 
 const reducer = (state = initialState, action) => {
@@ -8,6 +9,10 @@ const reducer = (state = initialState, action) => {
         case 'DECREMENT': return {...state, counter: state.counter - 1}
         case 'ADD': return {...state, counter: state.counter + action.val}
         case 'SUBTRACT': return {...state, counter: state.counter - action.val}
+        case 'STORE_RESULT': return {
+            ...state,
+            result: state.result.concat({id: new Date(), value: state.counter})
+        }
     }
     return state;
 }
